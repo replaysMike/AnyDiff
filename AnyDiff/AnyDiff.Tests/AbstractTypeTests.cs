20975075100ddfc95ac6a1e3d3ea686395ff91ca
Colin Bull <colinbul@gmail.com>
2020-04-21 09:52:48 +0100

    Added failing test

---
+++ AnyDiff/AnyDiff.Tests/AbstractTypeTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using AnyDiff.Tests.TestObjects;
+using AnyDiff.Tests.TestObjects.Abstract;
+using NUnit.Framework;
+
+namespace AnyDiff.Tests
+{
+    [TestFixture]
+    public class AbstractTypeTests
+    {
+        [Test]
+        public void If_BaseType_Is_Abstract_And_Both_Objects_Implement_Then_Should_Emit_Whole_Object_If_Types_Different()
+        {
+
+                var provider = new DiffProvider();
+
+                var obj1 = new TypeWithAbstractProperty { Id = "1", AbstractProp = new AbstractImplementation1() };
+                var obj2 = new TypeWithAbstractProperty { Id = "1", AbstractProp = new AbstractImplementation2() };
+                var diff = provider.ComputeDiff(obj1, obj2,ComparisonOptions.All | ComparisonOptions.AllowCompareDifferentObjects);
+                Assert.AreEqual(1, diff.Count);
+
+        }
+    }
+}
---
+++ AnyDiff/AnyDiff.Tests/TestObjects/Abstract/AbstractObject.cs
@@ -0,0 +1,29 @@
+﻿namespace AnyDiff.Tests.TestObjects.Abstract
+{
+    public abstract class AbstractObject
+    {
+        public virtual string Type
+        {
+            get => GetType().Name.ToLower();
+            set {}
+        }
+    }
+
+    public class AbstractImplementation1 : AbstractObject
+    {
+        public string Property1 { get; }
+        public int Property2 { get; }
+    }
+
+    public class AbstractImplementation2 : AbstractObject
+    {
+        public string Property2 { get; }
+        public int Property3 { get; }
+    }
+
+    public class TypeWithAbstractProperty
+    {
+        public string Id { get; set;  }
+        public AbstractObject AbstractProp { get; set; }
+    }
+}
